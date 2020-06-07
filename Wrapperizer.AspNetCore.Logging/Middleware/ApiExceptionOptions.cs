5ce615a23ec09031d6bbe36a7b96d40ae08472c9
Saeed Ganji <shahabganji@gmail.com>
2020-06-08 01:16:59 +0430

    init add logging basics

---
+++ Wrapperizer.AspNetCore.Logging/Middleware/ApiExceptionOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Wrapperizer.AspNetCore.Logging.Middleware
+{
+    public class ApiExceptionOptions
+    {
+        public Action<HttpContext, Exception, ApiError> AddResponseDetails { get; set; }  
+        public Func<Exception, LogLevel> DetermineLogLevel { get; set; }
+    }
+}
---
+++ Wrapperizer.AspNetCore.Logging/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿﻿using System.Collections.Generic;
+
+ namespace Wrapperizer.AspNetCore.Logging.Models
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public Dictionary<string, List<string>> UserClaims { get; set; }
+    }
+}
