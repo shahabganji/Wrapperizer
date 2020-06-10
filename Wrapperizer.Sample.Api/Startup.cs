8cd227cd4365daf1ac5e67f4f602c71f09eae93b
Saeed Ganji <shahabganji@gmail.com>
2020-06-11 00:50:37 +0430

    chore: add connections to app settings & DI

--- Wrapperizer.Sample.Api/Startup.cs
+++ Wrapperizer.Sample.Api/Startup.cs
@@ -1,18 +1,22 @@
 using System;
+using System.Data.SqlTypes;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
-using Serilog;
 using Wrapperizer.Extensions.DependencyInjection.Abstractions;
-using Wrapperizer.Extensions.Repositories.EfCore;
+using Wrapperizer.Sample.Configurations;
 using Wrapperizer.Sample.Infra.Persistence;
 using static HealthChecks.UI.Client.UIResponseWriter;
+using SqlServerConnection = Wrapperizer.Sample.Configurations.SqlServerConnection;
 
 namespace Wrapperizer.Sample.Api
 {
@@ -28,28 +32,18 @@ namespace Wrapperizer.Sample.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddHealthChecks()
-                .AddCheck("api", _ => HealthCheckResult.Healthy())
-                .AddRedis("localhost", "redis")
-                .AddMongoDb("mongodb://127.0.0.1:27017" , "orderdb","mongodb")
-                .AddSqlServer("Server=localhost; Database=fotokar; UID= sa; PWD=P@assw0rd")
-                .AddRabbitMQ(new Uri("amqp://guest:guest@localhost:5672/wrapperizer"),new SslOption(), "rabbitmq");
+            services.AddOptionsAndHealthChecks(Configuration);
             
             services.AddControllers();
 
-            services.AddEntityFrameworkInMemoryDatabase();
-
-            // services.AddDistributedMemoryCache();
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = "localhost";
-                options.InstanceName = "Wrapperizer.Api";
-            });
-
             services.AddDbContext<UniversityDbContext>((provider,builder) =>
             {
-                builder.UseInMemoryDatabase("sample_uni");
+                var sqlConnection = provider.GetRequiredService<SqlServerConnection>();
+
+                builder.UseSqlServer(sqlConnection.ConnectionString, options =>
+                {
+                    options.EnableRetryOnFailure(3);
+                });
             });
            
             services.AddWrapperizer()
@@ -95,6 +89,49 @@ namespace Wrapperizer.Sample.Api
                 endpoints.MapControllers();
             });
             
+        }
+    }
+
+    public static class CustomExtensions
+    {
+        public static IServiceCollection AddOptionsAndHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            
+            var sql = new SqlServerConnection();
+            configuration.Bind("Infra:Connections:Sql", sql);
+            services.Configure<SqlServerConnection>(instance => configuration.Bind("Infra:Connections:Sql", instance));
+            services.AddScoped(x => x.GetRequiredService<IOptionsSnapshot<SqlServerConnection>>().Value);
+            
+            var mongodb = new MongoDbConnection();
+            configuration.Bind("Infra:Connections:Mongodb", mongodb);
+            services.Configure<MongoDbConnection>(instance => configuration.Bind("Infra:Connections:Mongodb", instance));
+            services.AddScoped(x => x.GetRequiredService<IOptionsSnapshot<MongoDbConnection>>().Value);
+            
+            var rabbit = new RabbitMqConnection();
+            configuration.Bind("Infra:Connections:RabbitMQ", rabbit);
+            services.Configure<RabbitMqConnection>(instance => configuration.Bind("Infra:Connections:RabbitMQ", instance));
+            services.AddScoped(x => x.GetRequiredService<IOptionsSnapshot<MongoDbConnection>>().Value);
+            
+            var redis = new RedisCacheOptions();
+            configuration.Bind("Infra:Connections:Redis", redis);
+            services.Configure<RedisCacheOptions>(instance => configuration.Bind("Infra:Connections:Redis", instance));
+            services.AddScoped(x => x.GetRequiredService<IOptionsSnapshot<RedisCacheOptions>>().Value);
+            
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "localhost";
+                options.InstanceName = "Wrapperizer.Api";
+            });
+            
+            services.AddHealthChecks()
+                .AddCheck("api", _ => HealthCheckResult.Healthy())
+                .AddRedis(redis.Configuration, "redis")
+                .AddMongoDb(mongodb.ConnectionString , mongodb.Collection,"mongodb")
+                .AddSqlServer(sql.ConnectionString)
+                .AddRabbitMQ(rabbit.ConnectionUri,new SslOption(), "rabbitmq");
+
+            return services;
         }
     }
 }
