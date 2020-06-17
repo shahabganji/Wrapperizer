2893501466eb75a562418ec20645ea37d20143d4
Saeed Ganji <shahabganji@gmail.com>
2020-06-17 21:05:24 +0430

    add migrations for SoftDeleted

---
+++ Wrapperizer.Sample.Infra.Persistence/Migrations/20200617163408_SofDeleteForStudents.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wrapperizer.Sample.Infra.Persistence.Migrations
+{
+    public partial class SofDeleteForStudents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SoftDeleted",
+                schema: "uni",
+                table: "Students",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoftDeleted",
+                schema: "uni",
+                table: "Students");
+        }
+    }
+}
