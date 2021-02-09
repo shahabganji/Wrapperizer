008dd10d37f3616d24653524efa2ef108e628151
hamedfathi <noeffect@outlook.com>
2021-02-09 12:06:09 +0330

    feat: use List instead of IList with more functionalities.

--- core/Wrapperizer.Domain.Abstractions/FirstClassCollection.cs
+++ core/Wrapperizer.Domain.Abstractions/FirstClassCollection.cs
@@ -1,42 +1,53 @@
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Wrapperizer.Domain.Abstractions
 {
     public class FirstClassCollection<T>
     {
-        private readonly IList<T> _collection;
-
+        private readonly List<T> _list;
+        public int Count => _list.Count;
+        public IReadOnlyCollection<T> Items => _list.AsReadOnly();
         public FirstClassCollection()
         {
-            _collection = new List<T>();
+            _list = new List<T>();
         }
-
-        public void AddRange(IEnumerable<T> collection)
-        {
-            foreach (var item in collection)
-                Add(item);
-        }
-
         public void Add(T item)
         {
-            _collection.Add(item);
+            _list.Add(item);
         }
-
-        public void RemoveRange(IEnumerable<T> collection)
+        public void AddRange(IEnumerable<T> list)
         {
-            foreach (var item in collection)
-                Remove(item);
+            _list.AddRange(list);
         }
-
+        public IEnumerable<T> Filter(Expression<Func<T, bool>> predicate)
+        {
+            return _list.AsQueryable().Where(predicate);
+        }
+        public T Get(int itemIndex)
+        {
+            return _list[itemIndex];
+        }
+        public IEnumerable<T> Merge(params IEnumerable<T>[] lists)
+        {
+            var all = lists.SelectMany(x => x).ToList();
+            AddRange(all);
+            return _list;
+        }
         public void Remove(T item)
         {
-            _collection.Remove(item);
+            _list.Remove(item);
         }
-
-        public IReadOnlyCollection<T> GetAll()
+        public void RemoveRange(IEnumerable<T> list)
         {
-            return new ReadOnlyCollection<T>(_collection);
+            foreach (var item in list)
+                Remove(item);
+        }
+        public void Set(int itemIndex, T item)
+        {
+            _list[itemIndex] = item;
         }
     }
 }
